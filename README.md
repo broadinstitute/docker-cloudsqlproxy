77af918de7a57d3e0ba7919f58c6dcebb2a07bca
Henry Ferrara <hjferrara@bynara.com>
2018-07-26 15:11:32 -0400

    enabled a flag to allow using application default credentials added support for using default credentials and specifying multiple databases

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # docker-cloudsqlproxy
 Google CloudSQL proxy service
+
+This repo creates a wrapper (entrypoint) script to be used in the Google distributed
+CloudSQL proxy container.  The entrypoint.sh script autoruns when you run the
+docker container created using the Dockerfile in this repo.  All the settings for
+the CloudSQL proxy are "passed" in using environment variables - enabling a much
+simplier configuration.
+
+The script assumes one of two forms of service account authentication.
+  1) A service account json file that is mounted into the docker container
+     either under the default path (/etc/sqlproxy-service-account.json) or under
+     the path specified by the environment var (CLOUDSQL_CREDENTIAL_FILE)
+  2) Or using the "application" default service account.  This is either 
+     the service account used to create the GCE compute instance the docker 
+     is running on OR what ever the current user is authed as if they are 
+     running it on a non-GCE host.
+
+There are also two methods of setting the connection string used by the proxy.
+  1) Specify an explicit list of one or more databases using the environment 
+     variable (CLOUDSQL_CONNECTION_LIST).  This list must contain at least
+     one connection setting using the following format.  Multiple settings
+     should be comman seperated and all use UNIQUE port numbers (PORT)
+
+     GOOGLE_PROJECT:CLOUDSQL_ZONE:CLOUDSQL_INSTANCE=0.0.0.0:PORT
+
+     GOOGLE_PROJECT: Google project where cloudsql instance resides
+     CLOUDSQL_ZONE: Zone that the cloudsql instance resides
+     CLOUDSQL_INSTANCE: the exact name of the cloudsql instance
+     PORT: the TCP port number that the cloudsql proxy will listen on for 
+      connections to the cloudsql instance.
+
+  2) By specifying all of the following as environment variables.  Using this 
+     method supports only a single cloudsql instance.
+
+     GOOGLE_PROJECT: Google project where cloudsql instance resides
+     CLOUDSQL_ZONE: Zone that the cloudsql instance resides
+     CLOUDSQL_INSTANCE: the exact name of the cloudsql instance
+     PORT: the TCP port number that the cloudsql proxy will listen on for
+      connections to the cloudsql instance.
+
+Additional settings:
+
+  CLOUDSQL_MAXCONNS: set the max number of database connections the cloudsql
+    proxy will support.  Default is unlimited
+  CLOUDSQL_LOGGING: Logging setting.  Default is verbose logging
+
+  
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
+set -e
+set -u
+set -o pipefail
 
 # wrapper script for cloud_sql_proxy program
 
-# This script is written with the following assumptions:
+# By using this entrypoint script in the docker image it provides a much 
+#  simplier method of running the proxy.  Instead of having to specify the 
+#  exact sqlproxy command via the CMD or run - you are able to just set
+#  environment variables for the various settings
 #
-#   - ONLY connecting to a single Google CloudSQL instance.
-#   - ONLY utilizing service account authentication
+# This script assumes you will be using a service account as authenticaiton
+#  either by mounting the sevice account json into the docker container or 
+#  setting the appropriate role on the default service account
 
 MISSING=""
-version=0
-help=0
+CREDENTIALS=""
 CLOUDSQL_PROXY_CMD="/cloud_sql_proxy"
+
+# Init vars if not set in environment
+GOOGLE_PROJECT=${GOOGLE_PROJECT:-""}
+CLOUDSQL_ZONE=${CLOUDSQL_ZONE:-""}
+CLOUDSQL_INSTANCE=${CLOUDSQL_INSTANCE:-""}
+PORT=${PORT:-"3306"}
 
 # default to unlimited conns
 CLOUDSQL_MAXCONNS=${CLOUDSQL_MAXCONNS-0}
@@ -21,70 +33,117 @@ CLOUDSQL_CREDENTIAL_FILE=${CLOUDSQL_CREDENTIAL_FILE-"/etc/sqlproxy-service-accou
 # default to verbose logging
 CLOUDSQL_LOGGING=${CLOUDSQL_LOGGING-"-verbose"}
 
-# check if command line flag were passed
-if [ $# -gt 0 ]
-then
-   for arg in $@
-   do
-      # only support these flags
-      case $arg in
-        "-version") version=1
-            ;;
-        "-h") help=1
-            ;;
-      esac
-   done
-fi
+# flag to enable using the application default service credentials
+#  default is to not use default service account credentials
+CLOUDSQL_USE_DEFAULT_CREDENTIALS=${CLOUDSQL_USE_DEFAULT_CREDENTIALS-"0"}
 
-if [ $version -eq 1 ]
-then
-   # output cloudsql proxy version
-   ${CLOUDSQL_PROXY_CMD} -version
-   echo
-fi
+# CloudSQL instances to connect to
+CLOUDSQL_CONNECTION_LIST=${CLOUDSQL_CONNECTION_LIST:-""}
 
-if [ $help -eq 1 ]
-then
-   echo "Usage statement"
-
+# Usage message
+usage() {
+   echo "Usage message here"
    exit 1
+}
+
+# output version if you can
+version() {
+    if [ -x ${CLOUDSQL_PROXY_CMD} ]
+    then
+       echo
+       ${CLOUDSQL_PROXY_CMD} -version
+       echo
+    else
+       echo
+       echo "Unable to output version, can not execute proxy command (${CLOUDSQL_PROXY_CMD})"
+       echo
+    fi
+}
+
+# check if command line flag were passed
+TEMP=$(getopt -o vh --long version,help -- "$@")
+eval set -- "$TEMP"
+
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+        -v|--version)
+             version 
+             exit 1
+           ;;
+        -h|--help)
+             usage
+             exit 1
+           ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
+# if passing in connection string via CLOUDSQL_CONNECTION_LIST env var
+#  ignore env vars that are passed in that would be used to build
+#  connection string
+
+# No connection list set so must construct one from env vars
+if [ -z "${CLOUDSQL_CONNECTION_LIST}" ]
+then
+
+   # validate required vars are set.  check each one so can provide more
+   # specific error message indicating what is missing
+
+   if [ -z "${GOOGLE_PROJECT}" ]
+   then
+      MISSING="GOOGLE_PROJECT ${MISSING}"
+   fi
+
+   if [ -z "${CLOUDSQL_ZONE}" ]
+   then
+      MISSING="CLOUDSQL_ZONE ${MISSING}"
+   fi
+
+   if [ -z "${CLOUDSQL_INSTANCE}" ]
+   then
+      MISSING="CLOUDSQL_INSTANCE ${MISSING}"
+   fi
+
+   if [ ! -z "${MISSING}" ]
+   then
+      echo "The following REQUIRED environment variables were NOT set:"
+      echo
+      for miss in ${MISSING}
+      do
+         case $miss in 
+           "GOOGLE_PROJECT") echo "  GOOGLE_PROJECT: Google project name that CloudSQL instance resides"
+             ;;
+           "CLOUDSQL_ZONE") echo "  CLOUDSQL_ZONE: Google zone that instance resides in (us-central1-a, us-east1-b,..."
+             ;;
+           "CLOUDSQL_INSTANCE") echo "  CLOUDSQL_INSTANCE: Specific name of the CLoudSQL instance"
+             ;;
+         esac 
+      done
+      usage
+      echo ; echo "Exitting!"
+      exit 1
+   fi
+
+   # construct connection list from env vars
+   CLOUDSQL_CONNECTION_LIST="${GOOGLE_PROJECT}:${CLOUDSQL_ZONE}:${CLOUDSQL_INSTANCE}=tcp:0.0.0.0:${PORT}"
+
 fi
    
-
-# validate required vars are set
-
-if [ -z "${GOOGLE_PROJECT}" ]
+# determine what credentials will be used if flag not set then use a service
+#  account json file.
+if [ "${CLOUDSQL_USE_DEFAULT_CREDENTIALS}" -eq "0" ]
 then
-   MISSING="GOOGLE_PROJECT ${MISSING}"
+   if [ -r ${CLOUDSQL_CREDENTIAL_FILE} ]
+   then
+      CREDENTIALS="-credential_file=${CLOUDSQL_CREDENTIAL_FILE}"
+   else
+      echo "Unable to read credential file: (${CLOUDSQL_CREDENTIAL_FILE})! - Exitting"
+      echo 
+      exit 1
+   fi
 fi
 
-if [ -z "${CLOUDSQL_ZONE}" ]
-then
-   MISSING="CLOUDSQL_ZONE ${MISSING}"
-fi
-
-if [ -z "${CLOUDSQL_INSTANCE}" ]
-then
-   MISSING="CLOUDSQL_INSTANCE ${MISSING}"
-fi
-
-if [ ! -z "${MISSING}" ]
-then
-   echo "The following REQUIRED environment variables were NOT set:"
-   echo
-   for miss in ${MISSING}
-   do
-      case $miss in 
-        "GOOGLE_PROJECT") echo "  GOOGLE_PROJECT: Google project name that CloudSQL instance resides"
-          ;;
-        "CLOUDSQL_ZONE") echo "  CLOUDSQL_ZONE: Google zone that instance resides in (us-central1-a, us-east1-b,..."
-          ;;
-        "CLOUDSQL_INSTANCE") echo "  CLOUDSQL_INSTANCE: Specific name of the CLoudSQL instance"
-          ;;
-      esac 
-   done
-   echo ; echo "Exitting!"
-   exit 1
-fi
-
-exec ${CLOUDSQL_PROXY_CMD}  -instances=${GOOGLE_PROJECT}:${CLOUDSQL_ZONE}:${CLOUDSQL_INSTANCE}=tcp:0.0.0.0:3306 -max_connections=${CLOUDSQL_MAXCONNS} -credential_file=${CLOUDSQL_CREDENTIAL_FILE} ${CLOUDSQL_LOGGING}
+# launch proxy via exec to overlay existing shell
+exec ${CLOUDSQL_PROXY_CMD}  -max_connections=${CLOUDSQL_MAXCONNS} -instances=${CLOUDSQL_CONNECTION_LIST} ${CREDENTIALS}  ${CLOUDSQL_LOGGING}
